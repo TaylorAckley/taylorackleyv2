5e8493b7943051df0481a19a743847f557eee059
Taylor Ackley <taylorackley@gmail.com>
2017-12-30 11:35:49 -0800

    went to the trouble of adding a gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,70 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
+var uglify = require('gulp-uglify'),
+  notify = require('gulp-notify'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  plumber = require('gulp-plumber'),
+  serve = require('gulp-serve'),
+  connect = require('gulp-connect'),
+  rev = require('gulp-rev-append');
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
 ];
 
-gulp.task('sass', function() {
+jsPaths = [
+  'bower_components/jquery/dist/jquery.min.js',
+  'bower_components/what-input/what-input.js',
+  'bower_components/foundation-sites/dist/foundation.min.js',
+  'js/app.js'
+];
+
+
+gulp.task('serve', serve('.'));
+
+gulp.task('connect', function () {
+  connect.server({
+    root: '.',
+    livereload: true
+  });
+});
+
+gulp.task('bundleJS', function () {
+  return gulp.src(jsPaths)
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('js'))
+    .on('error', notify.onError("Error: <%= error.message %>"))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(uglify())
+    .on('error', notify.onError("Error: <%= error.message %>"))
+    .pipe(sourcemaps.write('/map'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('rev', function () {
+  gulp.src('./index.html')
+    .pipe(rev())
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+
+gulp.task('sass', function () {
   return gulp.src(['scss/app.scss', 'scss/global.scss'])
     .pipe($.sass({
-      includePaths: sassPaths
-    })
+        includePaths: sassPaths
+      })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
@@ -18,6 +72,8 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task('default', ['sass', 'connect', 'bundleJS', 'rev'], function () {
+  gulp.watch(['scss/**/*.scss'], ['sass', 'rev']);
+  gulp.watch(['js/app.js'], ['bundleJS', 'rev']);
+  gulp.watch(['*.html'], ['html', 'rev']);
 });
